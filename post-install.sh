37b257fe121037575d991f6e2060128ff47bddf9
renegade-d3v <jimmy.wilson11@gmail.com>
2023-12-23 16:24:31 +0200

    Rewrite script

--- post-install.sh
+++ post-install.sh
@@ -15,27 +15,28 @@ fi
 
 docker compose up -d
 
-if [ -f .env ]; then
-  export $(grep -v '^#' .env | xargs)
-fi
+DB_DATABASE="panda_db"
+echo "Введіть назву для БД (Назва за замовчуванням: $DB_DATABASE): "
+read user_input
+DB_DATABASE=${user_input:-$DB_DATABASE}
 
-# Запрашиваем у пользователя название базы данных
-read -p "Введіть назву для БД (Назва за замовчуванням: $DB_NAME): " user_input_db_name
-DB_NAME=${user_input_db_name:-$DB_NAME}
+DB_USERNAME="panda_app"
+echo "Введіть ім'я користувача (Ім'я за замовчуванням: $DB_USERNAME): "
+read user_input
+DB_USERNAME=${user_input:-$DB_USERNAME}
 
-read -p "Введіть ім'я користувача (Ім'я за замовчуванням: $DB_USER): " user_input_db_username
-DB_USER=${user_input_db_username:-$DB_USER}
+DB_PASSWORD=""
+echo "Введіть пароль для БД (Пароль за замовчуванням відсутній): "
+read -r user_input
+DB_PASSWORD=${user_input:-$DB_PASSWORD}
 
-read -p "Введіть пароль для БД (Пароль за замовчуванням відсутній $DB_PASSWORD): " user_input_db_pass
-DB_PASSWORD=${user_input_db_pass:-$DB_PASSWORD}
-
-if [ -z "$DB_NAME" ] || [ -z "$DB_USER" ] || [ -z "$DB_PASSWORD" ]; then
+if [ -z "$DB_DATABASE" ] || [ -z "$DB_USERNAME" ] || [ -z "$DB_PASSWORD" ]; then
   echo "Не вдалося отримати данні для створення БД"
   exit 1
 fi
 
-echo "DB_NAME=$DB_NAME" >> .env
-echo "DB_USER=$DB_USER" >> .env
+echo "DB_DATABASE=$DB_DATABASE" >> .env
+echo "DB_USERNAME=$DB_USERNAME" >> .env
 echo "DB_PASSWORD=$DB_PASSWORD" >> .env
 
 docker exec -it app-db mysql -uroot -proot -e "USE $DB_NAME" 2>/dev/null
@@ -43,9 +44,9 @@ docker exec -it app-db mysql -uroot -proot -e "USE $DB_NAME" 2>/dev/null
 if [ $? -eq 0 ]; then
   echo "База данных $DB_NAME уже существует."
 else
-  docker exec -it app-db mysql -uroot -proot -e "CREATE DATABASE $DB_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-  docker exec -it app-db mysql -uroot -proot -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
-  docker exec -it app-db mysql -uroot -proot -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%';"
+  docker exec -it app-db mysql -uroot -proot -e "CREATE DATABASE $DB_DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+  docker exec -it app-db mysql -uroot -proot -e "CREATE USER '$DB_USERNAME'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+  docker exec -it app-db mysql -uroot -proot -e "GRANT ALL ON $DB_DATABASE.* TO '$DB_USERNAME'@'%';"
   docker exec -it app-db mysql -uroot -proot -e "FLUSH PRIVILEGES;"
 fi
 
